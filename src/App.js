9b05a6fe22b0453893f4f1d2f506874d2050beb1
mixxx892 <64820209+mixxx892@users.noreply.github.com>
2022-08-27 20:00:32 +0300

    Simple registration form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        name={"firstName"}
+        placeholder={"First Name"}
+        {...register("firstName", {
+            required: true
+        })}
+      />
+      {errors.firstName && <p>This field is required</p>}
+
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        name={"lastName"}
+        placeholder={"Last Name"}
+        {...register("lastName", {
+          required: true
+        })}
+      />
+      {errors.lastName && <p>This field is required</p>}
+
+      <label htmlFor="password">Password</label>
+      <input
+        name={"password"}
+        placeholder={"Password"}
+        {...register("password", {
+          required: true
+        })}
+      />
+      {errors.password && <p>This field is required</p>}
+
+      <input type="submit" />
+    </form>
   );
 }
 
